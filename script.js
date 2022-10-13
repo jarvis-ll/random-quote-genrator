c1c6e8295b36c021f02551a33b76788173126c42
jainAashay <91877716+jainAashay@users.noreply.github.com>
2022-10-13 16:26:50 +0530

    script.js

--- script.js
+++ script.js
@@ -3,14 +3,26 @@ let author = document.getElementById("author");
 let btn = document.getElementById("btn");
 
 const url = "https://api.quotable.io/random";
-
+let a="";
 let getQuote = () => {
+  quote.innerText = "";
+  author.innerText = "";
   fetch(url)
     .then((data) => data.json())
     .then((item) => {
-      quote.innerText = item.content;
+      /*quote.innerText = item.content;*/
       author.innerText = item.author;
+      var string = item.content;
+var str = string.split("");
+var el = document.getElementById('quote');
+(function animate() {
+str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running); 
+var running = setTimeout(animate, 60);
+})();
+
+
     });
+    
 };
 
 window.addEventListener("load", getQuote);
